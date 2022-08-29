50589d36547f83904157752bae112eeb12909b9e
3talks <3talks123@gmail.com>
2022-08-29 20:47:38 +0545

    update 2.0

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,16 +1,16 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+import fifty from "../assets/img/fifty.png"
+import ninty from "../assets/img/ninty.png"
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
+import forty from "../assets/img/forty.png"
 import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
     const responsive = {
       superLargeDesktop: {
-        // the naming can be any, depends on you.
+       
         breakpoint: { max: 4000, min: 3000 },
         items: 5
       },
@@ -38,21 +38,18 @@ export const Skills = () => {
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
                             <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                                 <div className="item">
-                                    <img src={meter1} alt="Image" />
+                                    <img src={forty} alt="Image" />
                                     <h5>Web Development</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter2} alt="Image" />
-                                    <h5>Brand Identity</h5>
+                                    <img src={fifty} alt="Image" />
+                                    <h5>Graphic Design</h5>
                                 </div>
                                 <div className="item">
-                                    <img src={meter3} alt="Image" />
-                                    <h5>Logo Design</h5>
+                                    <img src={ninty} alt="Image" />
+                                    <h5>Repairing and Maintainence</h5>
                                 </div>
-                                <div className="item">
-                                    <img src={meter1} alt="Image" />
-                                    <h5>Web Development</h5>
-                                </div>
+                                
                             </Carousel>
                         </div>
                     </div>
